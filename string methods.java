8a029f6f7ebabb4b26e1147f365b6b984846c87f
Sainandan Palthi <sainandanpalthi135@gmail.com>
2025-03-29 16:28:20 +0530

    Add files via upload

---
+++ string methods.java
@@ -0,0 +1,17 @@
+class a
+{
+public static void main(String[] args)
+{
+String s="sai";
+System.out.println(s.toLowerCase());
+System.out.println(s.length());
+System.out.println(s.toUpperCase());
+System.out.println(s.replace('i','s'));
+System.out.println(s.trim());
+String s1="nandan";
+System.out.println(s1.equals(s));
+char s2[]={'n','a','n','d','a','n'};
+String p=new String(s2);
+System.out.println(p);
+}
+}
